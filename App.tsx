dc570cf6af6d84ec2fde2fe374c0b8285480b43d
vinukola <udaysekhar4@gmail.com>
2022-06-16 20:48:57 +0530

    1 file modified

--- App.tsx
+++ App.tsx
@@ -4,8 +4,7 @@ import './style.css';
 export default function App() {
   return (
     <div>
-      <h1>Hello uday!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <h1>Hello World!</h1>
     </div>
   );
 }
